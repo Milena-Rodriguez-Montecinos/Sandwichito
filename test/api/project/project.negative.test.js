046ce4d8a8f1f17890532f9c83b487bac8f1876c
Javier Choque <javier.dreivaj@gmail.com>
2021-10-28 01:45:54 -0400

    Fixed Tests

--- test/api/project/project.negative.test.js
+++ test/api/project/project.negative.test.js
@@ -2,33 +2,114 @@ import HttpRequestManager from "../../../src/common/api/http.request.manager";
 import endpointsList from "../../../src/resources/endpoints.json"
 import errors from "../../../src/resources/errors.json"
 import payloadList from "../../../src/resources/payloads/payloads.project.json"
+import logger from "../../../src/utils/logger";
 
 const projectsURI = endpointsList.endpoints.project.projects
+const projectByIdItemsURI = endpointsList.endpoints.project.projectByIdItems
+const projectByIdDoneItemsURI = endpointsList.endpoints.project.projectByIdDoneItems
+
 let projectByIdURI  = endpointsList.endpoints.project.projectById
 let id = '1234567890'
 let postID = ''
+let validId = ''
 
 describe("Project tests",() => {
     beforeAll(()=> {
-        ;
+        return HttpRequestManager.makeRequest('POST', projectsURI , payloadList.ProjectById.POST)
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+            validId = response.data.Id
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
     })
 
     afterAll(()=> {
-        ;
+        return HttpRequestManager.makeRequest('DELETE',projectByIdURI.replace('{id}',validId))
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
     })
 
-    test('Verify that I a 304 Invalid Project Id status code result when a GET request to the "projects/{id}.json" endpoint is executed', async()=>{
-        await HttpRequestManager.makeRequest('GET',projectByIdURI.replace('{id}',id))
+
+    test('Verify that a 304 Invalid Project Id status code result when a GET request to the "projects/{id}.json" endpoint is executed', async()=>{
+        return HttpRequestManager.makeRequest('GET',projectByIdURI.replace('{id}',id))
         .then(function(response) {
             expect(response.status).toBe(200)
             expect(response.statusText).toMatch("OK")
             expect(response.data).toEqual(errors.ErrorCode.NoAccessToProject)
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
 
-    },10000)
+    })
+
+    test('Verify that 200 OK status code result when GET items of an invalid project ID when request "/items.json" endpoint is executed', async()=>{
+        return HttpRequestManager.makeRequest('GET',projectByIdItemsURI.replace('{id}',id))
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).toEqual(errors.ErrorCode.NoAccessToProject)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+
+    })
+
+    test('Verify that 200 OK status code result when a GET done items of an invalid project ID when request "/doneitems.json" endpoint is executed', async()=>{
+        return HttpRequestManager.makeRequest('GET',projectByIdDoneItemsURI.replace('{id}',id))
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).toEqual(errors.ErrorCode.NoAccessToProject)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+
+    })
+    
+    test('Verify that shows the required error with a non valid ID when DELETE request "/projects.json" endpoint is executed', async()=>{
+        return HttpRequestManager.makeRequest('DELETE',projectByIdURI.replace('{id}',id))
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).toEqual(errors.ErrorCode.InvalidId)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+
+    })
+    
+    test('Verify that shows the required error with an invalid authentication when DELETE request "/projects.json" endpoint is executed', async()=>{
+        return HttpRequestManager.makeRequest('DELETE',projectByIdURI.replace('{id}',validId), false)
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+
+    })    
 
 })
--- test/api/project/projects.test.js
+++ test/api/project/projects.test.js
@@ -2,6 +2,7 @@ import HttpRequestManager from "../../../src/common/api/http.request.manager";
 import endpointsList from "../../../src/resources/endpoints.json"
 import errors from "../../../src/resources/errors.json"
 import payloadList from "../../../src/resources/payloads/payloads.project.json"
+import logger from "../../../src/utils/logger";
 
 const projectsURI = endpointsList.endpoints.project.projects
 let projectByIdURI  = endpointsList.endpoints.project.projectById
@@ -16,12 +17,13 @@ describe("Project tests",() => {
             expect(response.statusText).toMatch('OK')
             expect(response.data).not.toEqual(errors.Authentication)
             id = response.data.Id
+            postID = id;
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
-    },5000)
+    })
 
     afterAll(()=> {
         return HttpRequestManager.makeRequest('DELETE',projectByIdURI.replace('{id}',postID))
@@ -31,56 +33,54 @@ describe("Project tests",() => {
             expect(response.data).not.toEqual(errors.Authentication)
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
 
-    },5000)
+    })
 
     test('Verify that I a 200 ok status code result when a GET request to the "/projects.json" endpoint is executed', async()=>{
-        await HttpRequestManager.makeRequest('GET',projectsURI)
+        return HttpRequestManager.makeRequest('GET',projectsURI)
         .then(function(response) {
             expect(response.status).toBe(200)
             expect(response.statusText).toMatch('OK')
             expect(response.data).not.toEqual(errors.Authentication)
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
 
-    },10000)
+    })
 
     test('Verify that I a 200 ok status code result when a POST request to the "/projects.json" endpoint is executed', async()=>{
-        await HttpRequestManager.makeRequest('POST', projectsURI , payloadList.ProjectById.POST)
+        return HttpRequestManager.makeRequest('POST', projectsURI , payloadList.ProjectById.POST)
         .then(function(response) {
-            //console.log(response.data)
             expect(response.status).toBe(200)
             expect(response.statusText).toMatch('OK')
             expect(response.data).not.toEqual(errors.Authentication)
             postID = response.data.Id
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
 
     },5000)
 
     test('Verify that I a 200 ok status code result when a PUT request to the "/projects.json" endpoint is executed', async()=>{
-        await HttpRequestManager.makeRequest('PUT', projectByIdURI.replace('{id}',id) , payloadList.ProjectById.PUT)
+        return HttpRequestManager.makeRequest('PUT', projectByIdURI.replace('{id}',id) , payloadList.ProjectById.PUT)
         .then(function(response) {
-            //console.log(response.data)
             expect(response.status).toBe(200)
             expect(response.statusText).toMatch('OK')
             expect(response.data).not.toEqual(errors.Authentication)
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
 
-    },5000)
+    })
 
     test('Verify that I a 200 ok status code result when a DELETE request to the "/projects.json" endpoint is executed', async()=>{
         return HttpRequestManager.makeRequest('DELETE',projectByIdURI.replace('{id}',postID))
@@ -90,23 +90,23 @@ describe("Project tests",() => {
             expect(response.data).not.toEqual(errors.Authentication)
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
 
-    },5000)
+    })
 
     test('Verify that an error result when a GET request to the "/projects.json" endpoint is executed with invalid credentials', async()=>{
-        await HttpRequestManager.makeRequest('GET',projectsURI,'',false)
+        return HttpRequestManager.makeRequest('GET',projectsURI,'',false)
         .then(function(response) {
             expect(response.status).toBe(200)
             expect(response.statusText).toMatch('OK')
             expect(response.data).toEqual(errors.Authentication)
         })
         .catch(function (error) {
-            console.log(error)
+            logger.error(error);
             throw error
         })
-    },10000)
+    })
     
 })
