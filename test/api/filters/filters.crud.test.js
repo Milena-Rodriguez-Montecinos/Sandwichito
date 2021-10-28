009947a47274ce27e09ee1f8ab6820567b928859
Milena-Rodriguez-Montecinos <mile.raychel@gmail.com>
2021-10-28 06:04:42 -0400

    Adding more tests for crud testin

--- test/api/filters/filters.crud.test.js
+++ test/api/filters/filters.crud.test.js
@@ -24,4 +24,83 @@ describe("smoke test from Filters", () => {
             throw error;
         });
     });
+
+    test('Verify that a 200 OK status code is obtained when executing a GET request to the endpoint "/filters.json to return the filter with the given Id', async()=>{
+        return HttpRequestManager.makeRequest('GET', filtersURI.replace('{id}', afterPostId), '', 'user-two')
+
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+    })
+
+    test('Verify that a 200 OK status code is obtained when executing a GET request to the endpoint "/filters.json to return the filter with the given Id 0', async()=>{
+        return HttpRequestManager.makeRequest('GET', filtersURI, payload.ProjectById.GET.ID0 , 'user-two')
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+    })
+
+    test('Verify that a 200 OK status code is obtained when executing a GET request to the endpoint "filters/[id].json to return the filter with the given Id 1', async()=>{
+        return HttpRequestManager.makeRequest('GET', filtersURI, payload.ProjectById.GET.ID1 , 'user-two')
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+    })
+
+    test('Verify that a 200 OK status code is obtained when executing a GET request to the endpoint "filters/[id].json to return the filter with the given Id 2', async()=>{
+        return HttpRequestManager.makeRequest('GET', filtersURI, payload.ProjectById.GET.ID2 , 'user-two')
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+    })
+    
+    test('Verify that a 200 OK status code is obtained when executing a GET request to the endpoint "filters/[id].json to return the filter with the given Id 3', async()=>{
+        return HttpRequestManager.makeRequest('GET', filtersURI, payload.ProjectById.GET.ID3 , 'user-two')
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+    })
+
+    test('Verify that a 200 OK status code is obtained when executing a GET request to the endpoint "filters/[id]/items.json to return the items of a filter', async()=>{
+        return HttpRequestManager.makeRequest('GET', filtersURI)
+        .then(function(response) {
+            expect(response.status).toBe(200)
+            expect(response.statusText).toMatch('OK')
+            expect(response.data).not.toEqual(errors.Authentication)
+        })
+        .catch(function (error) {
+            logger.error(error);
+            throw error
+        })
+    })
 });
